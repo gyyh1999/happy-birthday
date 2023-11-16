4ff729742ae042e4d823374a18ea1dfa01063033
gyyh1999 <106085356+gyyh1999@users.noreply.github.com>
2023-11-16 21:55:44 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -221,8 +221,8 @@ const animationTimeline = () => {
       "-=2"
     )
     .from(".hat", 0.5, {
-      x: -100,
-      y: 350,
+      x: -150,
+      y: 400,
       rotation: -180,
       opacity: 0
     })
